b8183d7a0cd1ad00b08eaad72d128e1e31152e46
SanuthiVihansa <subasofts@gmail.com>
2023-05-28 21:05:38 +0530

    commit change color

--- Temperature App/src/App.jsx
+++ Temperature App/src/App.jsx
@@ -3,14 +3,18 @@ import { useState } from "react";
 
 const App = () => {
   const [temperatureValue, settemperatureValue] = useState(10);
+  const [temperatureColor,setTemperatureColor]=useState("cold")
+
+  
+  
   return (
     <div className="app-container">
       <div classname="temperature=display-container">
-        <div className="temperature-display">{temperatureValue}^C</div>
+        <div className={`temperature-display ${temperatureColor}`}>{temperatureValue} C</div>
       </div>
       <div className="button-container">
-        <button onClick={() => settemperatureValue}>+</button>
-        <button>-</button>
+        <button onClick={() => settemperatureValue(temperatureValue+1)}>+</button>
+        <button onClick={()=>settemperatureValue(temperatureValue-1)}>-</button>
       </div>
     </div>
   );
