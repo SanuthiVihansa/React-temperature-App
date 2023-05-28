9e08f942ef340b8c3f0154d92ed1a3be64b37461
SanuthiVihansa <subasofts@gmail.com>
2023-05-28 20:55:03 +0530

    commit formatting

--- Temperature App/src/App.jsx
+++ Temperature App/src/App.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
-
-const App=()=>{
+const App = () => {
+  const [temperatureValue, settemperatureValue] = useState(10);
   return (
     <div className="app-container">
       <div classname="temperature=display-container">
-        <div className ="temperature-display">10 C</div>
+        <div className="temperature-display">{temperatureValue}^C</div>
       </div>
       <div className="button-container">
-        <button>+</button>
+        <button onClick={() => settemperatureValue}>+</button>
         <button>-</button>
-
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
 export default App;
-
